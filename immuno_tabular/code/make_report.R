dfac7ee51503b2afc0339f75bb5c14a78ff75167
cyu <cyu@scharp.org>
2021-01-15 12:31:45 -0800

    To generate the report.pdf

---
+++ immuno_tabular/code/make_report.R
@@ -0,0 +1,14 @@
+library(rmarkdown)
+library(here)
+
+rmd <- "report.Rmd"
+outfile <- sprintf("%s-%s>", rmd, Sys.Date())
+
+# Render pdf
+render(
+  input = rmd,
+  output_format = "all", # Generate all outputs in YAML header
+  output_dir = here("immuno_tabular","output")
+)
+
+

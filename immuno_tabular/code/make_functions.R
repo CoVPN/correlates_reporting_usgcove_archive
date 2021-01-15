f761e4a6760a3c42f865bb245a637067b15d61f2
cyu <cyu@scharp.org>
2021-01-15 12:35:00 -0800

    Adding the parameters. Adding a new function for table formatting.

--- immuno_tabular/code/make_functions.R
+++ immuno_tabular/code/make_functions.R
@@ -1,3 +1,28 @@
+library(COVIDcorr)
+library(here)
+library(tidyverse)
+library(survey)
+library(knitr)
+library(kableExtra)
+
+indat <- COVIDcorr::dat.mock
+dataDate <- format(lubridate::mdy("01/12/2021"), "%B %d, %Y")
+
+bAb_lloq <- 34
+bAb_uloq <- 19136250
+nAb50_lloq <- 49
+nAb80_lloq <- 43
+
+# Variable Names by Assay
+bAb <- c("bindSpike", "bindRBD")
+pnAb <- c("pseudoneutid50", "pseudoneutid80")
+lnAb <- c("liveneutid50", "liveneutid80")
+
+visits <- c("B", "Day29", "Day57")
+bAb_v <-  levels(interaction(visits, bAb, sep=""))
+pnAb_v <- levels(interaction(visits, pnAb, sep=""))
+lnAb_v <- levels(interaction(visits, lnAb, sep=""))
+
 # Function to truncate at LLOQ and ULOQ
 setLOQ <- function(x, lloq=-Inf, uloq=Inf){
   lloq <- ifelse(is.infinite(lloq), lloq, log10(lloq))
@@ -13,7 +38,7 @@ setResponder <- function(data, bl, post, folds=c(2, 4), lloq, responderFR=4){
   names(foldsInd) <- paste0(post, "FR", folds)
   data <- cbind(data, foldsInd)
   data[, paste0(post, "Resp")] <- ifelse((data[, bl] < log10(lloq) & data[, post]>log10(lloq)) |
-                                         (data[, bl] >=log10(lloq) & data[, paste0(post, "FR", responderFR)]==1), 
+                                           (data[, bl] >=log10(lloq) & data[, paste0(post, "FR", responderFR)]==1), 
                                          1, 
                                          0)
   return(select_at(data, c(paste0(post, "Resp"), paste0(post, "FR", folds))))
@@ -31,7 +56,7 @@ demoSumm <- function(data, numVar, catVar){
     iCat <- summary(Subgroup)
     if (class(Subgroup)=="numeric") iCat <- round(iCat, 1)
     iCat
-    })
+  })
   retCat <- map_dfr(data[, catVar], .id="covariate", function(Subgroup){
     xlen <- length(Subgroup)
     iCat <- table(Subgroup) %>% data.frame() %>% mutate(Prop=Freq/xlen, Pct=sprintf("%.1f%%", 100*Prop))
@@ -39,3 +64,26 @@ demoSumm <- function(data, numVar, catVar){
   })
   return(list(numeric=retNum, category=retCat))
 }
+
+# Function to remove duplicate key rows from table outputs. 
+group_table <- function(data, keys){
+  # Sort data by keys
+  data <- data[do.call(order, data[ , keys, drop = FALSE]), ]
+  isfactor <- sapply(data, is.factor)
+  message(sprintf("Converting factors to character: %s",
+                  paste(names(isfactor)[isfactor], collapse = ", ")))
+  data[ ,isfactor] <- data.frame(lapply(data[, isfactor, drop = FALSE],
+                                        as.character), stringsAsFactors = FALSE)
+  
+  # Now find duplicates and replace with ""
+  groups <- Reduce(c, keys, accumulate = TRUE)
+  groups_dup <- lapply(groups, function(x) duplicated(data[ ,x]))
+  names(groups_dup) <- keys
+  lapply(seq(groups_dup), function(x) {
+    data[groups_dup[[x]], names(groups_dup[x])] <<- ""
+  })
+  
+  rownames(data) <- NULL
+  data
+  
+} # end group_table() 

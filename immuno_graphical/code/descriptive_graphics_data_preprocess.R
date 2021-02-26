93dbb023356685612fbbd93003e35720702115ed
KenLi93 <40015014+KenLi93@users.noreply.github.com>
2021-02-26 12:01:59 -0800

    Update descriptive_graphics_data_preprocess.R

--- immuno_graphical/code/descriptive_graphics_data_preprocess.R
+++ immuno_graphical/code/descriptive_graphics_data_preprocess.R
@@ -281,3 +281,6 @@ saveRDS(as.data.frame(dat.long.twophase.sample),
 saveRDS(as.data.frame(dat.twophase.sample),
   file = here("data_clean", "twophase_data.rds")
 )
+
+saveRDS(as.data.frame(dat.longer.cor.subset),
+        file = here("data_clean", "longer_cor_data.rds"))

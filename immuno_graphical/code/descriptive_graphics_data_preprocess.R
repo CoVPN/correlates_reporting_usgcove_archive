f7bd6c3f06ecf7c71957b3025872f5e6841bcb87
Benkeser <benkeser@emory.edu>
2021-02-26 11:09:58 -0500

    explicitly make race and ethnicity a factor, as it was in the previous mock data

--- immuno_graphical/code/descriptive_graphics_data_preprocess.R
+++ immuno_graphical/code/descriptive_graphics_data_preprocess.R
@@ -259,6 +259,11 @@ dat.long.twophase.sample$age_minority_label <-
       )
     )
   )
+dat.long.twophase.sample$ethnicity <- as.factor(dat.long.twophase.sample$ethnicity)
+dat.twophase.sample$ethnicity <- as.factor(dat.twophase.sample$ethnicity)
+dat.long.twophase.sample$race <- as.factor(dat.long.twophase.sample$race)
+dat.twophase.sample$race <- as.factor(dat.twophase.sample$race)
+
 
 saveRDS(as.data.frame(dat.long.twophase.sample),
   file = here("data_clean", "long_twophase_data.rds")

61775e276d5f53211e0bb4d968b9e5abf57417cf
Youyi Fong <youyifong@gmail.com>
2021-02-25 17:59:24 -0800

    add D29 results; better error handling because bootstrap throws an error with the new data

--- cor_coxph/code/params.R
+++ cor_coxph/code/params.R
@@ -1,15 +1,15 @@
 study.name="mock" # study.name is used in figure/table file names and printed in tables/figures as well
 
 # B=1e3 and numPerm=1e4 take 10 min to run with 30 CPUS. The results are saved in several .Rdata files.
-B=1e3 # number of bootstrap replicates
-numPerm=1e4 # number permutation replicates
+B=1e3 # number of bootstrap replicates 1e3
+numPerm=1e4 # number permutation replicates 1e4
 numCores=if(Sys.info()['sysname']=="Windows") 1 else parallel::detectCores()
 
 #assays=c("bindSpike","bindRBD","pseudoneutid50","liveneutmn50","pseudoneutid80")
 assays=c("bindSpike","bindRBD","pseudoneutid50","pseudoneutid80")
+.mfrow=if(length(assays)==4) c(2,2) else if(length(assays)==5) c(3,2) else stop("pls redefine .mfrows")
 
 trt.labels=c("Placebo","Vaccine")
 bstatus.labels=c("Baseline Neg","Pos")
 
-
-llods=c(bindSpike=20, bindRBD=20, pseudoneutid50=20, pseudoneutid80=20, liveneutmn50=62.16)
+llods=c(bindSpike=20, bindRBD=20, pseudoneutid50=10, pseudoneutid80=10, liveneutmn50=62)

4c09ce6b3ff4f96e251b06f93105157ec73a30cd
Lu <ylu2@fredhutch.org>
2021-02-26 10:39:43 -0800

    harmonize preprocessing code with Kendrick, remove data_prep.R and cor_graphics_violin_scatter.R, will create a new spec with Kendrick later

--- cor_graphical/code/cor_graphics_violin_scatter.R
+++ cor_graphical/code/cor_graphics_violin_scatter.R
@@ -20,16 +20,12 @@ trt <- c("Placebo","Vaccine")
 plots_ytitles <- c("Anti-Spike IgG (IU/ml)","Anti-RBD IgG (IU/ml)","Pseudovirus-nAb ID50","Pseudovirus-nAb ID80")
 plots_titles <- c("Binding Antibody to Spike","Binding Antibody to RBD","Pseudovirus Neutralization ID50","Pseudovirus Neutralization ID80")
 times <- list(c("Day 29","Day 57"), c("Day 1","Day 29","Day 57"))
-#### for Figure 3. intercurrent vs pp, case vs non-case, (Day 1) Day 29 Day 57, by if Age >=65 and if at risk
-groupby_vars3 <- c("Trt", "Bserostatus", "Event", "CohortInd", "time", "marker", "AgeInd", "HighRiskInd")
 
 ## load data 
-plot_dat <- readRDS(here("data_clean", "plot_data.rds"))
-plot_dat_long <- readRDS(here("data_clean", "plot_dat_long.rds"))
-plot_dat_long_stacked <- readRDS(here("data_clean", "plot_dat_long_stacked.rds"))  
-plot_dat_long_stacked_plot1 <- readRDS(here("data_clean", "plot_dat_long_stacked_plot1.rds"))  
+longer_cor_data <- readRDS(here("data_clean", "longer_cor_data.rds"))
+longer_cor_data_plot1 <- readRDS(here("data_clean", "longer_cor_data_plot1.rds"))  
 plot.25sample1 <- readRDS(here("data_clean", "plot.25sample1.rds"))  
-plot_dat_long_stacked_plot3 <- readRDS(here("data_clean", "plot_dat_long_stacked_plot3.rds"))  
+longer_cor_data_plot3 <- readRDS(here("data_clean", "longer_cor_data_plot3.rds"))  
 plot.25sample3 <- readRDS(here("data_clean", "plot.25sample3.rds"))
 
 #' A function to create a plot that shows violin + box or line + box figures
@@ -51,12 +47,13 @@ plot.25sample3 <- readRDS(here("data_clean", "plot.25sample3.rds"))
 #' @param col Colors options for the colby param
 #' @param inpanel.cex Font size for text within panels, response rate and "LLoQ"
 #' @param rate.y.pos Y coordinate for showing response rate
+#' @param axis.text.cex font size for x & y axis text
 #' @return A ggplot object for violin or line plots
 
 myplot <- function(dat, 
                    dat.sample,
                    x="time", 
-                   y="value2", 
+                   y="value", 
                    colby="cohort_event", 
                    shaby="cohort_event",
                    ylim=c(1,7), 
@@ -69,7 +66,8 @@ myplot <- function(dat,
                    facetopt="wrap",
                    col=c("#0AB7C9","#FF6F1B","#810094"),
                    inpanel.cex=6.5,
-                   rate.y.pos=7.7){
+                   rate.y.pos=7.7,
+                   axis.text.cex=19){
   
   p <- ggplot(data=dat, aes_string(x=x, y=y, color=colby, shape=shaby))
   
@@ -88,14 +86,16 @@ myplot <- function(dat,
   
   p <- p + 
     geom_text(aes(label=RespRate, x=time, y=rate.y.pos), color="black", size=inpanel.cex) +
-    geom_hline(aes(yintercept=LLoQ), linetype="dashed", color="gray") +
-    geom_text(aes(label="LLoQ", x=0.75, y=LLoQ), color="black", size=inpanel.cex) + 
+    geom_hline(aes(yintercept=LLoD), linetype="dashed", color="gray") +
+    geom_text(aes(label="LLoD", x=0.75, y=LLoD), color="black", size=inpanel.cex) + 
     scale_y_continuous(limits=ylim, breaks=ybreaks, labels=math_format(10^.x)) +
     labs(x=xtitle, y=ytitle, title=toptitle, color="Category", shape="Category") +
     scale_color_manual(values=col) +
     scale_shape_manual(values=c(16, 17, 15)) +
     theme(plot.margin = unit(c(0.25,0.25,0.25,0.25), "in"),
-          plot.title = element_text(hjust = 0.5))
+          plot.title = element_text(hjust = 0.5),
+          axis.text.x=element_text(size=axis.text.cex),
+          axis.text.y=element_text(size=axis.text.cex))
   return (p)
 }
 
@@ -109,72 +109,75 @@ for (typ in c("line","violin")) {
       for (k in 1:length(trt)) {
         for (t in 1:length(times)) {
           
-          p <- myplot(dat=subset(plot_dat_long_stacked_plot1, marker==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value2) & time %in% unlist(times[t])), 
-                       dat.sample=subset(plot.25sample1, marker==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value2) & time %in% unlist(times[t])), 
-                       ytitle=plots_ytitles[i],toptitle=plots_titles[i],
-                       type=typ,
-                       facetby=vars(cohort_event),
-                       ylim=c(1, 7.71),
-                       ybreaks=seq(1, 7),
-                       inpanel.cex=6.5,
-                       rate.y.pos=7.7
+          p <- myplot(dat=subset(longer_cor_data_plot1, assay==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value) & time %in% unlist(times[t])), 
+                      dat.sample=subset(plot.25sample1, assay==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value) & time %in% unlist(times[t])), 
+                      ytitle=plots_ytitles[i],toptitle=plots_titles[i],
+                      type=typ,
+                      facetby=vars(cohort_event),
+                      ylim=c(1, 7.71),
+                      ybreaks=seq(1, 7),
+                      inpanel.cex=6.5,
+                      rate.y.pos=7.7
+                      
           )
           file_name <- paste0(typ, "box_", gsub("bind","",gsub("pseudoneut","pnAb_",plots[i])), "_", trt[k], "_", gsub(" ","",bstatus[j]), "_",length(unlist(times[t])),"tp","_", study.name, ".pdf")
-          ggsave2(plot = p, filename = here("figs", file_name))
+          ggsave2(plot = p, filename = here("figs", file_name), width = 11.5, height = 11)
         }
       }
     }
   }
 }
 
-#### Figure 2. violin/line plot, intercurrent vs pp, case vs non-case, (Day 1) Day 29 Day 57, by if Age, HighRiskInd, Sex, Race and Ethnic group
+#### Figure 2. violin/line plot, intercurrent vs pp, case vs non-case, (Day 1) Day 29 Day 57, by if Age, HighRisk, Sex, Race and Ethnic group
 for (typ in c("line","violin")) {
   for (i in 1:length(plots)) {
     for (j in 1:length(bstatus)) {
       for (k in 1:length(trt)) {
         for (t in 1:length(times)) {
-          for (s in c("AgeInd","HighRiskInd","Sex","RaceEthnic","Dich_RaceEthnic")) {
+          for (s in c("age_geq_65_label","highrisk_label","sex_label","minority_label","Dich_RaceEthnic")) {
             
             # define response rate:
             # binding antibody, a positive response: concentration > 34 IU/ml, a negative response (≤ 34).
             # ID50/80 pseudo/live neut, a positive response: serum ID50 titer > 1:20 (log10(20)), a negative response as the complement
-            groupby_vars2 <- c("Trt", "Bserostatus", "Event", "CohortInd", "time", "marker", s)
-
-            plot_dat_long_stacked_plot2 <- 
-              plot_dat_long_stacked %>% group_by_at(groupby_vars2) %>%
+            groupby_vars2 <- c("Trt", "Bserostatus", "cohort_event", "time", "assay", s)
+            
+            longer_cor_data_plot2 <- 
+              longer_cor_data %>% group_by_at(groupby_vars2) %>%
               mutate(num = sum(response), 
                      denom=n(), 
                      RespRate = paste0(num,"/",denom,"=\n",round(num/denom*100, 1),"%"))
             
             # make subset for strata RaceEthnic and Dich_RaceEthnic, only present non-NA categories
-            if (s=="RaceEthnic") {
-              plot_dat_long_stacked_sub2 <- subset(plot_dat_long_stacked_plot2, RaceEthnic %in% c("White Non-Hispanic","Comm. of Color"))
+            if (s=="minority_label") {
+              longer_cor_data_sub2 <- subset(longer_cor_data_plot2, minority_label %in% c("White Non-Hispanic","Comm. of Color"))
               
             } else if(s=="Dich_RaceEthnic"){
-              plot_dat_long_stacked_sub2 <- subset(plot_dat_long_stacked_plot2, Dich_RaceEthnic %in% c("Hispanic or Latino","Not Hispanic or Latino"))
+              longer_cor_data_sub2 <- subset(longer_cor_data_plot2, Dich_RaceEthnic %in% c("Hispanic or Latino","Not Hispanic or Latino"))
               
-            } else {plot_dat_long_stacked_sub2 <- plot_dat_long_stacked_plot2}
+            } else {longer_cor_data_sub2 <- longer_cor_data_plot2}
             
             ## make another subsample datasets such that the jitter plot for each subgroup in each panel <= 25 data points
-            plot.25sample2 <-  plot_dat_long_stacked_sub2 %>% 
+            plot.25sample2 <-  longer_cor_data_sub2 %>% 
               group_by_at(groupby_vars2) %>%
               sample_n((ifelse(n()>=25, 25, n())), replace=F) %>% filter(time=="Day 57") %>% 
               ungroup() %>%
-              select(c("Ptid", groupby_vars3[!groupby_vars3 %in% "time"])) %>%
-              inner_join(plot_dat_long_stacked_sub2, by=c("Ptid", groupby_vars3[!groupby_vars3 %in% "time"]))
-
+              select(c("Ptid", groupby_vars2[!groupby_vars2 %in% "time"])) %>%
+              inner_join(longer_cor_data_sub2, by=c("Ptid", groupby_vars2[!groupby_vars2 %in% "time"]))
             
-            p <- myplot(dat=subset(plot_dat_long_stacked_sub2, marker==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value2) & time %in% unlist(times[t])), 
-                         dat.sample=subset(plot.25sample2, marker==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value2) & time %in% unlist(times[t])), 
-                         ytitle=plots_ytitles[i],toptitle=plots_titles[i],
-                         type=typ,
-                         facetby=as.formula(paste("~",s,"+cohort_event")),
-                         ylim=c(1, 8.2),
-                         ybreaks=seq(1, 7),
-                         inpanel.cex=5.5,
-                         rate.y.pos=7.9)
-            file_name <- paste0(typ, "box_", gsub("bind","",gsub("pseudoneut","pnAb_",plots[i])), "_", trt[k], "_", gsub(" ","",bstatus[j]), "_", gsub("Ind|High","",s), "_",length(unlist(times[t])),"tp","_", study.name, ".pdf")
-            ggsave2(plot = p, filename = here("figs", file_name))
+            
+            p <- myplot(dat=subset(longer_cor_data_sub2, assay==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value) & time %in% unlist(times[t])), 
+                        dat.sample=subset(plot.25sample2, assay==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value) & time %in% unlist(times[t])), 
+                        ytitle=plots_ytitles[i],toptitle=plots_titles[i],
+                        type=typ,
+                        facetby=as.formula(paste("~",s,"+cohort_event")),
+                        ylim=c(1, 8.2),
+                        ybreaks=seq(1, 7),
+                        inpanel.cex=5.5,
+                        rate.y.pos=7.9)
+            
+            s1 <- ifelse(s=="age_geq_65_label", "Age", ifelse(s=="highrisk_label", "Risk", ifelse(s=="sex_label","Sex", ifelse(s=="minority_label","RaceEthnic", ifelse(s=="Dich_RaceEthnic","Dich_RaceEthnic",NA)))))
+            file_name <- paste0(typ, "box_", gsub("bind","",gsub("pseudoneut","pnAb_",plots[i])), "_", trt[k], "_", gsub(" ","",bstatus[j]), "_", s1, "_",length(unlist(times[t])),"tp","_", study.name, ".pdf")
+            ggsave2(plot = p, filename = here("figs", file_name), width = 11.5, height = 11)
             
           }
         }
@@ -191,19 +194,19 @@ for (typ in c("line","violin")) {
       for (k in 1:length(trt)) {
         for (t in 1:length(times)) {
           
-          p <- myplot(dat=subset(plot_dat_long_stacked_plot3, marker==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value2) & time %in% unlist(times[t])), 
-                       dat.sample=subset(plot.25sample3, marker==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value2) & time %in% unlist(times[t])), 
-                       ytitle=plots_ytitles[i],toptitle=plots_titles[i],
-                       type=typ,
-                       facetby=as.formula("AgeInd_HighRiskInd~cohort_event"),
-                       ylim=c(1, 9.2),
-                       ybreaks=seq(1, 8, 2),
-                       facetopt = "grid",
-                       inpanel.cex=5,
-                       rate.y.pos=8.5
+          p <- myplot(dat=subset(longer_cor_data_plot3, assay==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value) & time %in% unlist(times[t])), 
+                      dat.sample=subset(plot.25sample3, assay==plots[i] & Bserostatus==bstatus[j] & Trt==trt[k] & !is.na(value) & time %in% unlist(times[t])), 
+                      ytitle=plots_ytitles[i],toptitle=plots_titles[i],
+                      type=typ,
+                      facetby=as.formula("age_risk_label~cohort_event"),
+                      ylim=c(1, 9.2),
+                      ybreaks=seq(1, 8, 2),
+                      facetopt = "grid",
+                      inpanel.cex=5,
+                      rate.y.pos=8.5
           )
           file_name <- paste0(typ, "box_", gsub("bind","",gsub("pseudoneut","pnAb_",plots[i])), "_", trt[k], "_", gsub(" ","",bstatus[j]), "_Age_Risk_", length(unlist(times[t])),"tp","_", study.name, ".pdf")
-          ggsave2(plot = p, filename = here("figs", file_name))
+          ggsave2(plot = p, filename = here("figs", file_name), width = 11.5, height = 13.5)
         }
       }
     }
@@ -211,26 +214,26 @@ for (typ in c("line","violin")) {
 }
 
 
-#### Figure 4. Scatter plot, marker vs. age in years, intercurrent vs pp, case vs non-case, (Day 1) Day 29 Day 57
+#### Figure 4. Scatter plot, assay vs. age in years, intercurrent vs pp, case vs non-case, (Day 1) Day 29 Day 57
 for (i in 1:length(plots)) {
   for (d in 1:length(times[[2]])) {
     for (c in c("Vaccine_BaselineNeg","all")) {
       
-      ds.tmp <- subset(plot_dat_long_stacked, marker==plots[i] & !is.na(value2) & time==times[[2]][d])
+      ds.tmp <- subset(longer_cor_data, assay==plots[i] & !is.na(value) & time==times[[2]][d])
       
       # subset for vaccine baseline neg arm
       if (c=="Vaccine_BaselineNeg"){ds.tmp <- subset(ds.tmp, Bserostatus=="Baseline Neg" & Trt=="Vaccine")}
-      ds.tmp$cohort_event <- factor(ds.tmp$cohort_event)      
+      
       y.breaks <- seq(1, ifelse(plots[i] %in% c("bindSpike","bindRBD"), 7, 5))
       y.lim=c(1, ifelse(plots[i] %in% c("bindSpike","bindRBD"), 7.7, 5))
       
-      p <- ggplot(ds.tmp, aes(x = Age, y = value2))
+      p <- ggplot(ds.tmp, aes(x = Age, y = value))
       
       # if show all four arms, multiple panels are needed
       if (c=="all") {p <- p + facet_wrap(~Bserostatus+Trt, nrow = 1)}
       
       p <- p + geom_point(size = 5, alpha = 1, aes(color = cohort_event, shape = cohort_event)) + 
-        geom_smooth(aes(group = Event), method = 'loess', se= TRUE, span = 1, color = "darkgray") + 
+        geom_smooth(aes(group = cohort_event), method = 'loess', se= TRUE, span = 1, color = "darkgray") + 
         scale_y_continuous(limits=y.lim, breaks=y.breaks, labels=math_format(10^.x)) +
         scale_x_continuous(breaks = seq(from=18, to=86, by=17)) +
         scale_color_manual(values = c("#0AB7C9","#FF6F1B","#810094")) +
@@ -240,10 +243,11 @@ for (i in 1:length(plots)) {
              color="Category", shape="Category") +
         theme(plot.margin = unit(c(1, 1, 1, 1), "cm"), 
               panel.grid = element_blank(),
-              plot.title = element_text(hjust = 0.5))
+              plot.title = element_text(hjust = 0.5),
+              axis.text.x = element_text(size=ifelse(c=="Vaccine_BaselineNeg", 27, 19)))
       
       file_name <- paste0("scatter_",gsub("bind","",gsub("pseudoneut","pnAb_",plots[i])),"_",c,"_",gsub(" ","",times[[2]][d]),"_", study.name, ".pdf")
-      ggsave2(plot = p, filename = here("figs", file_name))
+      ggsave2(plot = p, filename = here("figs", file_name), width = 11.5, height = 11)
       
     }
   }

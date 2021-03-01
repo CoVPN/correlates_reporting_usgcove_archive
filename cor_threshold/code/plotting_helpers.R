fdcab2446e0a002de0e717adf1bce1cd8865b462
Lars van der Laan <larsvanderlaan@berkeley.edu>
2021-03-01 12:03:47 -0800

    update thresh

--- cor_threshold/code/plotting_helpers.R
+++ cor_threshold/code/plotting_helpers.R
@@ -41,8 +41,8 @@ get_plot <- function(marker, simultaneous_CI = F, monotone = F) {
     scale_y_continuous(
       name = laby,
       sec.axis = sec_axis(~ . / scale_coef, name = "Reverse CDF"), n.breaks = 10
-    ) + geom_vline(xintercept = max_thresh, colour = "red", linetype = "longdash")  +
-    theme(plot.title = element_text(size = 15))#+  geom_text(aes(x=max_thresh *(1.01), label="No observed events", y=0.002), colour="black", angle=90, text=element_text(size=11))
+    ) + geom_vline(xintercept = max_thresh, colour = "red", linetype = "longdash") +
+    theme(plot.title = element_text(size = 15)) #+  geom_text(aes(x=max_thresh *(1.01), label="No observed events", y=0.002), colour="black", angle=90, text=element_text(size=11))
   append_end <- ""
   append_start <- "PLOT"
   folder <- ""
@@ -171,7 +171,7 @@ get_inverse_plot <- function(marker, simultaneous_CI = F) {
 
 
   # Save plot of inverse threshold response function
-  
+
   plot <- plot$plot + scale_y_continuous(
     labels = trans_format("ident", math_format(10^.x))
   ) + scale_x_continuous(n.breaks = 7) + xlab(main) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ylab(laby) + theme(plot.title = element_text(size = 16))

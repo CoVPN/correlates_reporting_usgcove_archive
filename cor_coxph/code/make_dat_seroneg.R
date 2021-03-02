9ea00ffda710f3f4ee870a564d3ffe48123b3195
Youyi Fong <youyifong@gmail.com>
2021-03-01 16:57:56 -0800

    updates

--- cor_coxph/code/make_dat_seroneg.R
+++ cor_coxph/code/make_dat_seroneg.R
@@ -1,9 +1,13 @@
 #-----------------------------------------------
 # obligatory to append to the top of each script
+# There is a bug on Windows that prevents renv from working properly. saved.system.libPaths provides a workaround:
+if (.Platform$OS.type == "windows") saved.system.libPaths=.libPaths()
 renv::activate(project = here::here(".."))
-source(here::here("..", "_common.R"))
-#-----------------------------------------------
-library(Hmisc)
+if (.Platform$OS.type == "windows") {
+    options(renv.config.install.transactional = FALSE)
+    renv::restore(library=saved.system.libPaths, prompt=FALSE) # for a quick test, add: packages="backports"
+    .libPaths(c(saved.system.libPaths, .libPaths()))
+} else renv::restore(prompt=FALSE)
 
 source(here::here("code", "params.R"))
 dat.mock <- read.csv(here::here("..", "data_clean", data_name))

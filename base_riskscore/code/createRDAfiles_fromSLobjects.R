41409ac3f270cb99bce82cbb2320b5ae5aee93a5
Bhavesh Borate <bborate@gizmok116.fhcrc.org>
2021-03-10 13:58:37 -0800

    code updates for risk score analysis

---
+++ base_riskscore/code/createRDAfiles_fromSLobjects.R
@@ -0,0 +1,86 @@
+#-----------------------------------------------
+# obligatory to append to the top of each script
+renv::activate(project = here::here(".."))
+source(here::here("..", "_common.R"))
+#-----------------------------------------------
+
+library(readr)
+require(tidyverse)
+library(here)
+
+# Create fancy/tidy screen names for use in tables and figures
+# @param avgs dataframe containing Screen, Learner, AUCs information as columns
+# @return object containing tidy screen names
+get_fancy_screen_names <- function(avgs){
+  return(avgs %>%
+           mutate(fancyScreen = case_when(Screen == "screen_highcor_random_plus_exposure_All" ~ "highcor_random",
+                                          Screen == "screen_glmnet_plus_exposure_All" ~ "glmnet",
+                                          Screen == "screen_univariate_logistic_pval_plus_exposure_All" ~ "univar_logistic_pval",
+                                          Screen == "screen_all_plus_exposure_All" ~ "all",
+                                          TRUE ~ as.character(Screen)))
+         )
+}
+
+
+# Drop seeds/fits that returned any error
+# @param dat object containing all 10 fits (as lists) from the CV.Superlearner with folds and auc information 
+# @return object upon dropping any fit that returned an error
+drop_seeds_with_error <- function(dat){
+  newdat <- vector(mode = "list", length = 1)
+  j = 1
+  for (i in 1:10){
+    if( typeof(dat[[i]][1]) == "list" ){
+      newdat[[j]] = dat[[i]]
+      j = j + 1
+    }
+  }
+  newdat
+}
+
+
+# Convert SL object to SL results dataframe
+# @param dat object containing all 10 fits (as lists) from the CV.Superlearner with folds and auc information
+# @return dataframe containing CV-AUCs
+convert_SLobject_to_Slresult_dataframe <- function(dat) {
+
+  # Remove any iteration seeds that returned an error!
+  newdat = drop_seeds_with_error(dat)
+    
+  # if (is.null(newdat[[1]])) {
+  #   return( read.csv("empty_df.csv", stringsAsFactors = FALSE) %>% select(-X) %>% as_tibble() )
+  # }
+
+  if (!is.null(newdat[[1]])) {
+    as_tibble(do.call(rbind.data.frame, lapply(newdat, function(x) x$aucs))) %>%
+      #filter(!is.na(ci_ll) | !is.na(ci_ul)) %>%    # drop learners that have NA for ci_ul or ci_ll for certain seeds!
+      group_by(Learner, Screen) %>%
+      summarise(AUC = mean(AUC), ci_ll = mean(ci_ll), ci_ul = mean(ci_ul), .groups = 'drop') %>%
+      arrange(-AUC) %>%
+      mutate(AUCstr = paste0(format(round(AUC, 3), nsmall=3), " [", format(round(ci_ll, 3), nsmall=3), ", ", format(round(ci_ul, 3), nsmall=3), "]"),
+             Learner = as.character(Learner),
+             Screen = as.character(Screen),
+             LearnerScreen = paste(Learner, Screen)) %>%
+      get_fancy_screen_names() %>%
+      rename(Screen_fromRun = Screen,
+             Screen = fancyScreen)
+  }
+}
+
+
+
+# Read in SL objects from folder, get AUCs in a dataframe
+# @param data_file RDS file containing all 10 fits from the CV.Superlearner with folds and auc information
+# @param trt string containing treatment arm (placebo or vaccine)
+# @return dataframe containing CV-AUCs
+readin_SLobjects_fromFolder <- function(data_file, trt){
+  readRDS(data_file) %>%
+    convert_SLobject_to_Slresult_dataframe() %>%
+    mutate(trt = trt)
+}
+
+
+# Read CV.SL object and save AUCs
+data_file <- here("results", "cvsl_riskscore_cvaucs.rds")  
+risk_placebo_cvaucs <- readin_SLobjects_fromFolder(data_file, trt = "placebo") 
+save(risk_placebo_cvaucs, file = here("results", "cvsl_risk_placebo_cvaucs.rda"))
+
---
+++ base_riskscore/code/run_cvsl_riskscore.R
@@ -0,0 +1,137 @@
+#-----------------------------------------------
+# obligatory to append to the top of each script
+renv::activate(project = here::here(".."))
+source(here::here("..", "_common.R"))
+#-----------------------------------------------
+
+# load required libraries and functions
+library(tidyverse)
+library(here)
+library(methods)
+library(SuperLearner)
+library(e1071)
+library(glmnet)
+library(kyotil)
+library(argparse)
+library(vimp)
+library(nloptr)
+library(RhpcBLASctl)
+library(conflicted)
+conflicted::conflict_prefer("filter", "dplyr")
+conflict_prefer("summarise", "dplyr")
+library(mice)
+
+# Risk score analysis: Superlearner code requires computing environment with more than 10 cores!
+num_cores <- parallel::detectCores()
+if(num_cores < 10) stop("Number of cores on this computing environment are less than 10! Superlearner code needs atleast 11 cores to run smoothly.")
+
+# Define code version to run
+run_demo <- TRUE  # the demo version is simpler and runs faster!
+run_prod <- FALSE # the prod version runs SL with the complete set of diverse learners and takes more time (around 5-7 hrs on a cluster machine!)
+
+source(here("code", "sl_screens.R")) # set up the screen/algorithm combinations
+source(here("code", "utils.R")) # get CV-AUC for all algs
+
+############ SETUP INPUT ####################### 
+# Read in data file
+dat.mock <- read.csv(here::here("..", "data_clean", data_name))
+inputFile <- dat.mock
+
+# Identify the risk demographic variable names that will be used to compute the risk score
+risk_vars <- c("MinorityInd", "EthnicityHispanic", "EthnicityNotreported", "EthnicityUnknown", 
+               "Black", "Asian", "NatAmer", "PacIsl", "WhiteNonHispanic", "Multiracial", 
+               "Other", "Notreported", "Unknown", "HighRiskInd", "Sex", "Age", "BMI")
+
+# Identify the endpoint variable
+endpoint <- "EventIndPrimaryD57"
+################################################    
+
+# Consider only placebo data for risk score analysis
+dat.ph1 <- inputFile %>%
+  filter(Perprotocol == 1 & Trt == 0) %>%
+  # Keep only variables to be included in risk score analyses
+  select(Ptid, Trt, all_of(endpoint), all_of(risk_vars)) %>%  
+  # Drop any observation with NA values in Ptid, Trt, or endpoint!
+  drop_na(Ptid, Trt, all_of(endpoint))
+  
+# Derive maxVar: the maximum number of variables that will be allowed by SL screens in the models.  
+np <- sum(dat.ph1 %>% select(matches(endpoint)))
+maxVar <- max(20, floor(np/20))
+
+# Remove any risk_vars that have fewer than 10 1s 
+dat.ph1 <- drop_riskVars_with_fewer_1s(dat.ph1, risk_vars)
+
+# Remove any risk_vars with more than 5% missing values. Impute the missing values for other risk variables using mice package! 
+dat.ph1 <- drop_riskVars_with_high_total_missing_values(dat.ph1, risk_vars)
+
+# Update risk_vars
+risk_vars <- dat.ph1 %>% select(-Ptid, -Trt, -all_of(endpoint)) %>% colnames()
+
+X_covars2adjust <-  dat.ph1 %>%
+  select(all_of(risk_vars))
+ 
+# Save ptids to merge with predictions later
+risk_placebo_ptids <- dat.ph1 %>% select(Ptid, all_of(endpoint)) 
+
+# Impute missing values in any variable included in risk_vars using the mice package!
+print("Make sure data is clean before conducting imputations!")
+X_covars2adjust <- impute_missing_values(X_covars2adjust, risk_vars)
+
+# Scale X_covars2adjust to have mean 0, sd 1 for all vars
+for (a in colnames(X_covars2adjust)) {
+  X_covars2adjust[[a]] <- scale(X_covars2adjust[[a]],
+                                center = mean(X_covars2adjust[[a]], na.rm = T),  
+                                scale = sd(X_covars2adjust[[a]], na.rm = T))    
+}
+
+X_riskVars <- X_covars2adjust
+Y = dat.ph1 %>% pull(endpoint)
+
+# set up outer folds for cv variable importance; do stratified sampling
+V_outer <- 5
+
+# set up inner folds based off number of cases 
+if(np <= 30){
+  V_inner <- length(Y) - 1
+} else {
+  V_inner <- 5  
+}
+
+## ensure reproducibility
+set.seed(20210217)
+seeds <- round(runif(10, 1000, 10000)) # average over 10 random starts
+
+##solve cores issue
+library(RhpcBLASctl)
+blas_get_num_procs()
+blas_set_num_threads(1)
+print(blas_get_num_procs())
+stopifnot(blas_get_num_procs()==1)
+
+
+fits <- parallel::mclapply(seeds, FUN = run_cv_sl_once, 
+                           Y = Y, 
+                           X_mat = X_riskVars, 
+                           family = "binomial",
+                           sl_lib = SL_library,
+                           method = "method.CC_nloglik",
+                           cvControl = list(V = V_outer, stratifyCV = TRUE),
+                           innerCvControl = list(list(V = V_inner)),
+                           scale = "identity", 
+                           vimp = FALSE,
+                           mc.cores = num_cores
+)
+
+cvaucs <- list()
+cvfits <- list()
+
+for(i in 1:length(seeds)) {
+  cvaucs[[i]] = fits[[i]]$cvaucs
+  cvfits[[i]] = fits[[i]]$cvfits
+}
+
+saveRDS(cvaucs, here("results", "cvsl_riskscore_cvaucs.rds"))
+save(cvfits, file = here("results", "cvsl_riskscore_cvfits.rda"))
+save(risk_placebo_ptids, file = here("results", "risk_placebo_ptids.rda"))
+save(run_demo, run_prod, Y, X_riskVars, weights, inputFile, risk_vars, endpoint, maxVar, V_outer, file = here("results", "objects_for_running_SL.rda"))
+

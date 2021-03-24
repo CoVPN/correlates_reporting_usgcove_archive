6586ee93ebc1f0586ae6f2556f26708292590b90
Nima Hejazi <nh@nimahejazi.org>
2021-03-24 13:05:30 -0700

    tweak coxph params; lower bootstraps and permutations in Travis

--- cor_coxph/code/params.R
+++ cor_coxph/code/params.R
@@ -1,6 +1,13 @@
 # B=1e3 and numPerm=1e4 take 10 min to run with 30 CPUS for D57 CoR
 B <- 1e3 # number of bootstrap replicates 1e3
 numPerm <- 1e4 # number permutation replicates 1e4
-numCores <- if(Sys.info()['sysname'] == "Windows") 1 else parallel::detectCores()
+numCores <- unname(ifelse(Sys.info()["sysname"] == "Windows",
+                          1, future::availableCores()))
 
-.mfrow=if(length(assays)==4) c(2,2) else if(length(assays)==5) c(3,2) else stop("pls redefine .mfrows")
+if (length(assays) == 4) {
+  .mfrow <- c(2, 2)
+} else if (length(assays) == 5) {
+  .mfrow <- c(3, 2)
+} else {
+  stop("Please re-define variable .mfrows")
+}

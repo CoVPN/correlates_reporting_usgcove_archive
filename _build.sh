03afec30a4b588110efbe1d02a59c9ba28d6712d
Benkeser <benkeser@emory.edu>
2021-03-29 16:34:54 -0400

    no errors when files no found

--- _build.sh
+++ _build.sh
@@ -6,4 +6,4 @@ Rscript -e "bookdown::render_book(input = 'index_$1.Rmd', config_file = '_bookdo
 pdflatex -halt-on-error -output-directory _report_$1 _report_$1/covpn_correlates_$1.tex
 pdflatex -halt-on-error -output-directory _report_$1 _report_$1/covpn_correlates_$1.tex
 pdflatex -halt-on-error -output-directory _report_$1 _report_$1/covpn_correlates_$1.tex
-rm _report_$1/*.aux _report_$1/*.log _report_$1/*.lof _report_$1/*.tex _report_$1/*.aux _report_$1/*.toc _report_$1/*.lot
+rm -f _report_$1/*.aux _report_$1/*.log _report_$1/*.lof _report_$1/*.tex _report_$1/*.aux _report_$1/*.toc _report_$1/*.lot

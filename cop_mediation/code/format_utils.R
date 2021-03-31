31864795e2be3c37badc46533b4be90aa4072f6b
Benkeser <benkeser@emory.edu>
2021-03-31 15:47:48 -0400

    add initial code for cop mediation

---
+++ cop_mediation/code/format_utils.R
@@ -0,0 +1,32 @@
+#' @param x numeric vector of point estimate, cil, ciu
+#' @param digits number of digits to round to
+format_ci <- function(x, digits = 3){
+  paste0(sprintf(paste0("%.", digits, "f"), x[1]), " (",
+         sprintf(paste0("%.", digits, "f"), x[2]), ", ",
+         sprintf(paste0("%.", digits, "f"), x[3]), ")")
+}
+
+#' Format rows for a table as est (95% CI)
+#' @param fit a natmed2 fit
+format_row <- function(fit, digits = 3){
+	# extract risk and effect estimates
+  risk_est <- fit$risk[,1]
+  total_eff <- fit$eff["Total", "one_step_est"]
+  direct_eff <- fit$eff["Direct", "one_step_est"]
+  indirect_eff <- fit$eff["Indirect", "one_step_est"]
+
+  # compute proportion mediated
+  prop_med <- 1 - log(direct_eff) / log(total_eff)
+	g <- matrix(c(log(direct_eff)/(log(total_eff))^2*1/risk_est[1], log(indirect_eff)/(log(total_eff))^2*1/risk_est[2],
+              -1/(risk_est[3]*log(total_eff)), nrow = 1))
+	se_prop_med <- t(g) %*% fit$cov %*% g
+	cil <- prop_med - qnorm(0.975) * se_prop_med
+  ciu <- prop_med + qnorm(0.975) * se_prop_med
+
+  this_row <- c(
+    format_ci(fit$eff["Direct", 2:4], digits = digits),
+    format_ci(fit$eff["Indirect", 2:4], digits = digits),
+    format_ci(c(prop_med, cil, ciu), digits = digits)
+  )
+  return(this_row)
+}

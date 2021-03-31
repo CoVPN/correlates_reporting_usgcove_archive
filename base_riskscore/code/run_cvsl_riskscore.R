68303dacc313dd2d4a709b9763c0c585991cd49e
Youyi Fong <youyifong@gmail.com>
2021-03-31 11:56:50 -0700

    Update run_cvsl_riskscore.R

--- base_riskscore/code/run_cvsl_riskscore.R
+++ base_riskscore/code/run_cvsl_riskscore.R
@@ -8,173 +8,133 @@ if (.Platform$OS.type == "windows") .libPaths(c(paste0(Sys.getenv ("R_HOME"), "/
 source(here::here("..", "_common.R"))
 #-----------------------------------------------
 
-## load libraries and source files #############################################
-library(cvAUC)
-library(conflicted)
+# load required libraries and functions
 library(tidyverse)
-library(vimp)
-library(kyotil)
-library(grid)
-library(gridExtra)
-library(cowplot)
 library(here)
-conflict_prefer("filter", "dplyr")
+library(methods)
+library(SuperLearner)
+library(e1071)
+library(glmnet)
+library(kyotil)
+library(argparse)
+library(vimp)
+library(nloptr)
+library(RhpcBLASctl)
+library(conflicted)
+conflicted::conflict_prefer("filter", "dplyr")
 conflict_prefer("summarise", "dplyr")
-source(here("code", "utils.R"))
-method <- "method.CC_nloglik" # since SuperLearner relies on this to be in GlobalEnv
-ggplot2::theme_set(theme_cowplot())
+library(mice)
 
-load(file = here("output", "objects_for_running_SL.rda"))
-rm(Y, X_riskVars, weights, maxVar)
+# Define code version to run
+# the demo version is simpler and runs faster!
+# the production version runs SL with a diverse set of learners
+run_prod <- TRUE
 
-load(file = here("output", "cvsl_risk_placebo_cvaucs.rda"))
+# get utility files
+source(here("code", "sl_screens.R")) # set up the screen/algorithm combinations
+source(here("code", "utils.R")) # get CV-AUC for all algs
 
-######## Table of demographic variables used to derive the risk score ##########
-dat <- read.csv(here::here("..", "data_clean", data_name)) %>%
+############ SETUP INPUT #######################
+# Read in data file
+dat_cleaned <- read.csv(here::here("..", "data_clean", data_name))
+inputFile <- dat_cleaned
+
+# Identify the risk demographic variable names that will be used to compute the risk score
+risk_vars <- c(
+  "MinorityInd", "EthnicityHispanic", "EthnicityNotreported",
+  "EthnicityUnknown", "Black", "Asian", "NatAmer", "PacIsl",
+  "WhiteNonHispanic", "Multiracial", "Other", "Notreported", "Unknown",
+  "HighRiskInd", "Sex", "Age", "BMI"
+)
+
+# Identify the endpoint variable
+endpoint <- "EventIndPrimaryD57"
+################################################
+
+# Consider only placebo data for risk score analysis
+dat.ph1 <- inputFile %>%
+  filter(Perprotocol == 1 & Trt == 0) %>%
+  # Keep only variables to be included in risk score analyses
+  select(Ptid, Trt, all_of(endpoint), all_of(risk_vars)) %>%
+  # Drop any observation with NA values in Ptid, Trt, or endpoint!
+  drop_na(Ptid, Trt, all_of(endpoint))
+
+# Derive maxVar: the maximum number of variables that will be allowed by SL screens in the models.
+np <- sum(dat.ph1 %>% select(matches(endpoint)))
+maxVar <- max(20, floor(np / 20))
+
+# Remove any risk_vars that have fewer than 10 1s
+dat.ph1 <- drop_riskVars_with_fewer_1s(dat.ph1, risk_vars)
+
+# Remove any risk_vars with more than 5% missing values. Impute the missing
+# values for other risk variables using mice package!
+dat.ph1 <- drop_riskVars_with_high_total_missing_values(dat.ph1, risk_vars)
+
+# Update risk_vars
+risk_vars <- dat.ph1 %>%
+  select(-Ptid, -Trt, -all_of(endpoint)) %>%
+  colnames()
+
+X_covars2adjust <- dat.ph1 %>%
   select(all_of(risk_vars))
 
-dat %>%
-  map(~ sum(is.na(.))) %>%
-  as.data.frame() %>%
-  t() %>%
-  as.data.frame() %>%
-  tibble::rownames_to_column(var = "Variable Name") %>%
-  mutate(
-    V1 = paste0(V1, "/", nrow(dat), " (", format(round((V1 / nrow(dat)) * 100, 1), nsmall = 1), "%)"),
-    Definition = case_when(
-      `Variable Name` == "Age" ~ "Age at enrollment in years, between 18 and 85",
-      `Variable Name` == "Sex" ~ "Sex assigned at birth (1=female, 0=male)",
-      `Variable Name` == "BMI" ~ "BMI at enrollment (kg/m^2)",
-      `Variable Name` == "MinorityInd" ~ "Baseline covariate underrepresented minority status (1=minority, 0=non-minority)",
-      `Variable Name` == "EthnicityHispanic" ~ "Indicator ethnicity = Hispanic (0 = Non-Hispanic)",
-      `Variable Name` == "EthnicityNotreported" ~ "Indicator ethnicity = Not reported (0 = Non-Hispanic)",
-      `Variable Name` == "EthnicityUnknown" ~ "Indicator ethnicity = Unknown (0 = Non-Hispanic)",
-      `Variable Name` == "Black" ~ "Indicator race = Black (0 = White)",
-      `Variable Name` == "Asian" ~ "Indicator race = Asian (0 = White)",
-      `Variable Name` == "NatAmer" ~ "Indicator race = American Indian or Alaska Native (0 = White)",
-      `Variable Name` == "PacIsl" ~ "Indicator race = Native Hawaiian or Other Pacific Islander (0 = White)",
-      `Variable Name` == "WhiteNonHispanic" ~ "Indicator race = White or Caucasian (1 = White)",
-      `Variable Name` == "Multiracial" ~ "Indicator race = Multiracial (0 = White)",
-      `Variable Name` == "Other" ~ "Indicator race = Other (0 = White)",
-      `Variable Name` == "Notreported" ~ "Indicator race = Not reported (0 = White)",
-      `Variable Name` == "Unknown" ~ "Indicator race = unknown (0 = White)",
-      `Variable Name` == "HighRiskInd" ~ "Baseline covariate high risk pre-existing condition (1=yes, 0=no)"
-    ),
-    Comments = ""
-  ) %>%
-  rename(`Total missing values` = V1) %>%
-  select(`Variable Name`, Definition, `Total missing values`, Comments) %>%
-  write.csv(here("output", "risk_vars.csv"))
+# Save ptids to merge with predictions later
+risk_placebo_ptids <- dat.ph1 %>% select(Ptid, all_of(endpoint))
 
-######## learner-screens #######################################################
-caption <- "All learner-screen combinations (28 in total) used as input to the superlearner."
+# Impute missing values in any variable included in risk_vars using the mice package!
+print("Make sure data is clean before conducting imputations!")
+X_covars2adjust <- impute_missing_values(X_covars2adjust, risk_vars)
 
-if (run_prod) {
-  tab <- risk_placebo_cvaucs %>%
-    filter(!Learner %in% c("SL", "Discrete SL")) %>%
-    select(Learner, Screen) %>%
-    mutate(
-      Screen = fct_relevel(Screen, c(
-        "all", "glmnet", "univar_logistic_pval",
-        "highcor_random"
-      )),
-      Learner = as.factor(Learner),
-      Learner = fct_relevel(Learner, c(
-        "SL.mean", "SL.glm", "SL.glm.interaction",
-        "SL.glmnet", "SL.gam", 
-        "SL.xgboost", "SL.ranger"#, "SL.cforest", "SL.bayesglm"
-      ))
-    ) %>%
-    arrange(Learner, Screen) %>%
-    distinct(Learner, Screen) %>%
-    rename("Screen*" = Screen)
-} else {
-  tab <- risk_placebo_cvaucs %>%
-    filter(!Learner %in% c("SL", "Discrete SL")) %>%
-    select(Learner, Screen) %>%
-    mutate(
-      Screen = fct_relevel(Screen, c(
-        "all", "glmnet", "univar_logistic_pval",
-        "highcor_random"
-      )),
-      Learner = as.factor(Learner),
-      Learner = fct_relevel(Learner, c("SL.mean", "SL.glm"))
-    ) %>%
-    arrange(Learner, Screen) %>%
-    distinct(Learner, Screen) %>%
-    rename("Screen*" = Screen)
+# Scale X_covars2adjust to have mean 0, sd 1 for all vars
+for (a in colnames(X_covars2adjust)) {
+  X_covars2adjust[[a]] <- scale(X_covars2adjust[[a]],
+    center = mean(X_covars2adjust[[a]], na.rm = T),
+    scale = sd(X_covars2adjust[[a]], na.rm = T)
+  )
 }
 
-tab %>% write.csv(here("output", "learner-screens.csv"))
+X_riskVars <- X_covars2adjust
+Y <- dat.ph1 %>% pull(endpoint)
 
-######## SLperformance-plac ####################################################
-caption <- "Performance of Superlearner and all learner-screen combinations (CV-AUCs with 95\\% CIs) for risk score analyses using placebo group and EventIndPrimaryD57 as outcome. Constraint of np/20 is applied to all learners such that no more than 6 input variables were allowed in any model."
+# set up outer folds for cv variable importance; do stratified sampling
+V_outer <- 5
 
-sl.perf <- risk_placebo_cvaucs %>%
-  mutate(AUCstr = ifelse(AUC %in% tail(sort(AUC), 1), paste0(AUCstr, "*"), AUCstr)) %>%
-  select(Learner, Screen, AUCstr)
-
-sl.perf %>% write.csv(here("output", "SLperformance-plac.csv"))
-
-################################################################################
-# Forest plots for risk_placebo model, yd57 endpoint
-options(bitmapType = "cairo")
-if (run_prod) {
-  png(file = here("figs", "risk_placebo_cvaucs.png"),
-      width = 1000, height = 1100)
-  top_learner <- make_forest_plot_prod(risk_placebo_cvaucs)
+# set up inner folds based off number of cases
+if (np <= 30) {
+  V_inner <- length(Y) - 1
 } else {
-  png(file = here("figs", "risk_placebo_cvaucs.png"),
-      width = 1000, height = 700)
-  top_learner <- make_forest_plot_demo(risk_placebo_cvaucs)
+  V_inner <- 5
 }
-grid.arrange(top_learner$top_learner_nms_plot, top_learner$top_learner_plot,
-             ncol = 2)
-dev.off()
 
-################################################################################
-# plot ROC curve and pred.Prob with SL, Discrete SL and top 2 best-performing individual Learners
-top2_plac <- bind_rows(
-  risk_placebo_cvaucs %>% arrange(-AUC) %>%
-    filter(!Learner %in% c("SL", "Discrete SL")) %>%
-    dplyr::slice(1:2),
-  risk_placebo_cvaucs %>%
-    filter(Learner == "SL"),
-  risk_placebo_cvaucs %>%
-    filter(Learner == "Discrete SL")
-) %>%
-  mutate(LearnerScreen = ifelse(Learner == "SL", "Super Learner",
-                                ifelse(Learner == "Discrete SL", Learner,
-                                       paste0(Learner, "_", Screen_fromRun))))
+## solve cores issue
+library(RhpcBLASctl)
+blas_get_num_procs()
+blas_set_num_threads(1)
+print(blas_get_num_procs())
+stopifnot(blas_get_num_procs() == 1)
 
-# Get cvsl fit and extract cv predictions
-load(file = here("output", "cvsl_riskscore_cvfits.rda"))
-pred <- get_cv_predictions(cv_fit = cvfits[[1]], cvaucDAT = top2_plac)
+set.seed(20210216)
 
-# plot ROC curve
-options(bitmapType = "cairo")
-png(file = here("figs", "ROCcurve_riskscore_plac.png"),
-    width = 750, height = 750)
-p1 <- plot_roc_curves(pred, cvaucDAT = top2_plac)
-print(p1)
-dev.off()
+# run super learner ensemble
+fits <- run_cv_sl_once(
+  Y = Y,
+  X_mat = X_riskVars,
+  family = "binomial",
+  sl_lib = SL_library,
+  method = "method.CC_nloglik",
+  cvControl = list(V = V_outer, stratifyCV = TRUE),
+  innerCvControl = list(list(V = V_inner)),
+  scale = "identity",
+  vimp = FALSE
+)
 
-# plot pred prob plot
-options(bitmapType = "cairo")
-png(file = here("figs", "predProb_riskscore_plac.png"),
-    width = 1000, height = 1200)
-p2 <- plot_predicted_probabilities(pred)
-print(p2)
-dev.off()
+cvaucs <- list()
+cvaucs[[1]] <- fits$cvaucs
+cvfits <- list()
+cvfits[[1]] <- fits$cvfits
 
-# Use SuperLearner to generate risk scores!
-load(file = here("output", "risk_placebo_ptids.rda"))
-plac <- bind_cols(
-  risk_placebo_ptids,
-  pred %>% filter(Learner == "SL") %>% select(pred, AUCchar)
-) %>%
-  mutate(risk_score = log(pred / (1 - pred)))
-
-write.csv(plac, here("output", "placebo_ptids_with_riskscores.csv"),
-          row.names = FALSE)
-rm(cvfits, pred, p1, p2)
+saveRDS(cvaucs, here("output", "cvsl_riskscore_cvaucs.rds"))
+save(cvfits, file = here("output", "cvsl_riskscore_cvfits.rda"))
+save(risk_placebo_ptids, file = here("output", "risk_placebo_ptids.rda"))
+save(run_prod, Y, X_riskVars, weights, inputFile, risk_vars, endpoint, maxVar,
+     V_outer, file = here("output", "objects_for_running_SL.rda"))

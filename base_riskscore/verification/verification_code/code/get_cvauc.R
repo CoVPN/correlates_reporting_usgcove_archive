64c6d6c3dc14812f12e2349978f9b0b9c02904a2
David Benkeser <benkeser@emory.edu>
2021-04-06 21:22:26 -0400

    move towards cvauc ci calculation using vimp

--- base_riskscore/verification/verification_code/code/get_cvauc.R
+++ base_riskscore/verification/verification_code/code/get_cvauc.R
@@ -6,11 +6,35 @@ renv::activate(project = here::here("..", "..", ".."))
 source(here::here("..", "..", "..", "_common.R"))
 #-----------------------------------------------
 
-library(cvAUC)
+library(vimp)
 library(dplyr)
 
+# Using SL fit object and scale = "identity", 
+# get the CV-AUC estimate, SE and CI’s for each algorithm 
+# (each individual learner, SL and discrete SL) as follows:
+# For each of the 5 outer folds, run vimp::measure_auc  
+# (vimp package version 2.1.10) to obtain cv-auc and eif 
+# (estimated influence function)
+# The cv-auc estimate is the average of cv-auc estimates 
+# across the 5 outer folds.
+# Get estimate of standard error using vimp::vimp_se function
+# Get CI’s using vimp::vimp_ci function.
+
 cv_auc_from_sl <- function(sl){
-  	out <- cvAUC::ci.cvAUC(predictions = sl$SL.predict,
+  V <- length(sl$folds)
+  pt_est <- rep(NA, V)
+  se_est <- rep(NA, V)
+  for(v in 1:V){
+    auc_v <- vimp::measure_auc(fitted_values = sl$SL.predict[sl$folds[[v]]],
+                               y = sl$Y[sl$folds[[v]]])
+    pt_est[v] <- auc_v$point_est
+    se_est[v] <- as.numeric(vimp::vimp_se(auc_v))
+  }
+  cvauc_pt_est <- mean(pt_est)
+  cvauc_se <- sqrt( mean(se_est^2) )
+  ci <- vimp::vimp_ci(cvauc_pt_est, cvauc_se)
+
+    	out <- cvAUC::ci.cvAUC(predictions = sl$SL.predict,
   	                       labels = sl$Y,
   	                       folds = sl$folds)
   	# replace ci with CI on logit scale

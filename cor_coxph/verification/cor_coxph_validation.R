d37a2de55539094f167a0df619a155c28e1e3c50
Bhavesh Borate <bborate@rhino02.fhcrc.org>
2021-04-15 09:33:44 -0700

    more additions to code on cor_coxph validation

--- cor_coxph/verification/cor_coxph_validation.R
+++ cor_coxph/verification/cor_coxph_validation.R
@@ -7,21 +7,22 @@ source(here::here("..", "_common.R"))
 # load required libraries and functions
 library(tidyverse)
 library(here)
-library(methods)
-library(SuperLearner)
+# library(methods)
+# library(SuperLearner)
 library(glmnet)
 library(kyotil)
 library(conflicted)
 conflicted::conflict_prefer("filter", "dplyr")
 conflict_prefer("summarise", "dplyr")
-library(mice)
+# library(mice)
 library(marginalizedRisk)
 library(tools) 
 library(survey)
-library(parallel)
-library(forestplot)
+# library(parallel)
+# library(forestplot)
 library(Hmisc)
 library(xtable) 
+library(broom)
 
 # Read in data file
 inputFile <- read.csv(here::here("..", "data_clean", "practice_data_with_riskscore.csv")) 
@@ -32,29 +33,72 @@ markerDay = 57
 # create data subsets
 dat <- inputFile %>% filter(Trt == 1 & Bserostatus == 0)
 
+# This function takes marker name as string, and the design.
+# It returns the HR for the marker, CI and p.value
+getHR_D57_continuous_marker <- function(marker, design, data, group){
+  fm = as.formula(paste("Surv(EventTimePrimaryD57, EventIndPrimaryD57) ~ ", marker, " + MinorityInd + HighRiskInd + risk_score"))
+  
+  fit <- survey::svycoxph(fm, design=design)
+  
+  tidy(fit, conf.int = T, exponentiate = T) %>% data.frame() %>%
+    select(term, estimate, conf.low, conf.high, p.value) %>%
+    filter(!term %in% c("MinorityInd", "HighRiskInd", "risk_score")) %>%
+    rename(marker = term) %>%
+    mutate(events = sum(data$EventIndPrimaryD57),
+           n = length(data$EventIndPrimaryD57),
+           group = group) 
+}
+
+
+
+getHR_D57_categorical_marker <- function(marker, design, data, group){
+  
+  # table(data %>% pull(all_of(marker)))
+  # Add events and n for lower, middle and upper
+  data_lower <- data %>% filter(get(marker) == (data %>% pull(marker) %>% levels())[1])
+  data_middle <- data %>% filter(get(marker) == (data %>% pull(marker) %>% levels())[2])
+  data_upper <- data %>% filter(get(marker) == (data %>% pull(marker) %>% levels())[3])
+  # data_lower <- data %>% filter(str_detect(get(marker), '-Inf'))
+  # data_middle <- data %>% filter(!str_detect(get(marker), 'Inf'))
+  # data_upper <- data %>% filter(str_detect(get(marker), ', Inf]') | str_detect(marker, '3.11'))
+
+  fm = as.formula(paste("Surv(EventTimePrimaryD57, EventIndPrimaryD57) ~ ", marker, " + MinorityInd + HighRiskInd + risk_score"))
+  
+  fit <- survey::svycoxph(fm, design=design)
+  
+  dat <- tidy(fit, conf.int = T, exponentiate = T) %>% data.frame() %>%
+    select(term, estimate, conf.low, conf.high, p.value) %>%
+    filter(!term %in% c("MinorityInd", "HighRiskInd", "risk_score")) %>%
+    rename(marker = term) %>%
+    add_row(marker = "Lower", estimate = 1, conf.low = NA, conf.high = NA, p.value = NA) %>%
+    mutate(marker_cut = c("Middle", "Upper", "Lower")) %>%
+    arrange(marker_cut) %>%
+    mutate(events = ifelse(marker_cut == "Lower", sum(data_lower$EventIndPrimaryD57),
+                           ifelse(marker_cut == "Middle", sum(data_middle$EventIndPrimaryD57),
+                                  sum(data_upper$EventIndPrimaryD57))),
+           n = ifelse(marker_cut == "Lower", length(data_lower$EventIndPrimaryD57),
+                      ifelse(marker_cut == "Middle", length(data_middle$EventIndPrimaryD57),
+                             length(data_upper$EventIndPrimaryD57))),
+           group = group)
+  
+}
 
 ################################################## 
 # Day 57 analysis
-
 dat.D57 <- dat %>% filter(!is.na(wt))
 
-dat.D57.design <- twophase(id=list(~Ptid, ~Ptid),strata=list(NULL, ~Wstratum),
-                           data=dat.D57, subset=~TwophasesampInd)
-
-fit <- survey::svycoxph(Surv(EventTimePrimaryD57, EventIndPrimaryD57) ~ Day57bindSpike + MinorityInd + HighRiskInd + risk_score, design=dat.D57.design)
-
-# Define trichotomized version of the markers: Day57bindSpike, Day57bindRBD, Day57pseudoneutid50, Day57pseudoneutid80
+# Define trichotomized version of the markers
+# trichotomize Day57bindSpike
 vec_cutpoints <- c(-Inf, Hmisc::wtd.quantile(dat.D57$Day57bindSpike, weights = dat.D57$wt, probs = c(1/3, 2/3)), Inf)
 if((vec_cutpoints[[2]] == min(dat.D57$Day57bindSpike, na.rm = T)) | 
    (vec_cutpoints[[3]] == max(dat.D57$Day57bindSpike, na.rm = T)) |
    (vec_cutpoints[[2]] == vec_cutpoints[[3]])){
   dat.D57$Day57bindSpikecat <- Hmisc::cut2(dat.D57$Day57bindSpike, breaks = 3)
 } else {
-    dat.D57$Day57bindSpikecat <- Hmisc::cut2(dat.D57$Day57bindSpike, vec_cutpoints)}
+  dat.D57$Day57bindSpikecat <- Hmisc::cut2(dat.D57$Day57bindSpike, vec_cutpoints)}
 rm(vec_cutpoints)
 
-
-
+# trichotomize Day57bindRBD
 vec_cutpoints <- c(-Inf, Hmisc::wtd.quantile(dat.D57$Day57bindRBD, weights = dat.D57$wt, probs = c(1/3, 2/3)), Inf)
 if((vec_cutpoints[[2]] == min(dat.D57$Day57bindRBD, na.rm = T)) | 
    (vec_cutpoints[[3]] == max(dat.D57$Day57bindRBD, na.rm = T)) |
@@ -64,9 +108,7 @@ if((vec_cutpoints[[2]] == min(dat.D57$Day57bindRBD, na.rm = T)) |
   dat.D57$Day57bindRBDcat <- Hmisc::cut2(dat.D57$Day57bindRBD, vec_cutpoints)}
 rm(vec_cutpoints)
 
-
-
-
+# trichotomize Day57pseudoneutid50
 vec_cutpoints <- c(-Inf, Hmisc::wtd.quantile(dat.D57$Day57pseudoneutid50, weights = dat.D57$wt, probs = c(1/3, 2/3)), Inf)
 if((vec_cutpoints[[2]] == min(dat.D57$Day57pseudoneutid50, na.rm = T)) | 
    (vec_cutpoints[[3]] == max(dat.D57$Day57pseudoneutid50, na.rm = T)) |
@@ -76,9 +118,7 @@ if((vec_cutpoints[[2]] == min(dat.D57$Day57pseudoneutid50, na.rm = T)) |
   dat.D57$Day57pseudoneutid50cat <- Hmisc::cut2(dat.D57$Day57pseudoneutid50, vec_cutpoints)}
 rm(vec_cutpoints)
 
-
-
-
+# trichotomize Day57pseudoneutid80
 vec_cutpoints <- c(-Inf, Hmisc::wtd.quantile(dat.D57$Day57pseudoneutid80, weights = dat.D57$wt, probs = c(1/3, 2/3)), Inf)
 if((vec_cutpoints[[2]] == min(dat.D57$Day57pseudoneutid80, na.rm = T)) | 
    (vec_cutpoints[[3]] == max(dat.D57$Day57pseudoneutid80, na.rm = T)) |
@@ -88,19 +128,52 @@ if((vec_cutpoints[[2]] == min(dat.D57$Day57pseudoneutid80, na.rm = T)) |
   dat.D57$Day57pseudoneutid80cat <- Hmisc::cut2(dat.D57$Day57pseudoneutid80, vec_cutpoints)}
 rm(vec_cutpoints)
 
-
-
 dat.D57 <- dat.D57 %>%
   mutate(Day57bindSpikecat = factor(Day57bindSpikecat),
          Day57bindRBDcat = factor(Day57bindRBDcat),
          Day57pseudoneutid50cat = factor(Day57pseudoneutid50cat),
          Day57pseudoneutid80cat = factor(Day57pseudoneutid80cat))
 
+##########################################
+
 
 dat.D57.design <- twophase(id=list(~Ptid, ~Ptid),strata=list(NULL, ~Wstratum),
                            data=dat.D57, subset=~TwophasesampInd)
 
-fitcat <- survey::svycoxph(Surv(EventTimePrimaryD57, EventIndPrimaryD57) ~ Day57bindSpikecat + MinorityInd + HighRiskInd + risk_score, design=dat.D57.design)
+tab1 <- getHR_D57_continuous_marker(marker = "Day57bindSpike", design = dat.D57.design, data = dat.D57, group = "All baseline negative, vaccine") %>%
+  bind_rows(getHR_D57_continuous_marker(marker = "Day57bindRBD", design = dat.D57.design, data = dat.D57, group = "All baseline negative, vaccine"),
+            getHR_D57_continuous_marker(marker = "Day57pseudoneutid50", design = dat.D57.design, data = dat.D57, group = "All baseline negative, vaccine"),
+            getHR_D57_continuous_marker(marker = "Day57pseudoneutid80", design = dat.D57.design, data = dat.D57, group = "All baseline negative, vaccine"))
+
+tab2 <- getHR_D57_categorical_marker(marker = "Day57bindSpikecat", design = dat.D57.design, data = dat.D57, group = "All baseline negative, vaccine") %>%
+  bind_rows(getHR_D57_categorical_marker(marker = "Day57bindRBDcat", design = dat.D57.design, data = dat.D57, group = "All baseline negative, vaccine"),
+            getHR_D57_categorical_marker(marker = "Day57pseudoneutid50cat", design = dat.D57.design, data = dat.D57, group = "All baseline negative, vaccine"),
+            getHR_D57_categorical_marker(marker = "Day57pseudoneutid80cat", design = dat.D57.design, data = dat.D57, group = "All baseline negative, vaccine")) 
+
+################################################## 
+# Age >= 65
+# create data subsets
+dat <- inputFile %>% filter(Trt == 1 & Bserostatus == 0 & Age >= 65)
+dat.D57 <- dat %>% filter(!is.na(wt))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ################################################## 
 # Day 29 analysis

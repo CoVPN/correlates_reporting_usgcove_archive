f833efbbbf50103d0d2066f502fe208d8c30f03d
Youyi Fong <youyifong@gmail.com>
2021-04-27 17:26:35 -0700

    censoring binding not nABb

--- data_clean/make_dat_proc.R
+++ data_clean/make_dat_proc.R
@@ -280,12 +280,20 @@ for (a in c("bindSpike", "bindRBD", "bindN")) {
 # censoring values below LLOD
 ###############################################################################
 
+# llod censoring
 for (a in assays.includeN) {
   for (t in if(has29) c("B", "Day29", "Day57") else c("B", "Day57") ) {
     dat_proc[[t %.% a]] <- ifelse(dat_proc[[t %.% a]] < log10(llods[a]), log10(llods[a] / 2), dat_proc[[t %.% a]])
-    #dat_proc[[t %.% a]] <- ifelse(dat_proc[[t %.% a]] > log10(uloqs[a]), log10(uloqs[a]    ), dat_proc[[t %.% a]])
   }
 }
+
+# uloq censoring for binding only
+for (a in c("bindSpike", "bindRBD", "bindN")) {
+  for (t in if(has29) c("B", "Day29", "Day57") else c("B", "Day57") ) {
+    dat_proc[[t %.% a]] <- ifelse(dat_proc[[t %.% a]] > log10(uloqs[a]), log10(uloqs[a]    ), dat_proc[[t %.% a]])
+  }
+}
+
 
 
 ###############################################################################

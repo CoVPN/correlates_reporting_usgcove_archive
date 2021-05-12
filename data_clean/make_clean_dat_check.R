bb3bfe7bef0bc06978619f45cd98402d88416706
Youyi Fong <youyifong@gmail.com>
2021-05-12 12:09:57 -0700

    adding to variables_with_no_missing in both make_clean_dat_check and make_raw_dat_check

--- data_clean/make_clean_dat_check.R
+++ data_clean/make_clean_dat_check.R
@@ -23,14 +23,14 @@ for (a in assays_plusN) {
   for (t in if(has29) c("B", "Day29", "Day57") else c("B", "Day57") ) {
     pass <- all(dat_clean[[paste0(t,a)]] >= log10(llods[a] / 2), na.rm = TRUE)
     if(!pass){
-    	failed_llod_check <- c(failed_llod_check, paste0(t,a))
+        failed_llod_check <- c(failed_llod_check, paste0(t,a))
     }
   }
 }
 
 if(length(failed_llod_check) > 1){
-	stop(paste0("Values of assays less than LLOD / 2 for: ", 
-	            paste(failed_llod_check, sep = ", ")))
+    stop(paste0("Values of assays less than LLOD / 2 for: ", 
+                paste(failed_llod_check, sep = ", ")))
 }
 
 ## missing values in variables that should have no missing values
@@ -44,27 +44,28 @@ variables_with_no_missing <-
       "SubcohortInd",
       "age.geq.65",
       "TwophasesampInd","TwophasesampInd.2",
-      "MinorityInd"
+      "MinorityInd",
+      "EarlyendpointD57", "EarlyendpointD29"
       )
 
 failed_variables_missing <- failed_variables_01 <- NULL
 for(variable in variables_with_no_missing){
-	pass <- all(!is.na(dat_clean[[variable]]))
-	if(!pass){
-		failed_variables_missing <- c(failed_variables_missing, variable)
-	}
-	pass <- all(dat_clean[[variable]] %in% c(0,1))
-	if(!pass){
-		failed_variables_01 <- c(failed_variables_01, variable)
-	}
+    pass <- all(!is.na(dat_clean[[variable]]))
+    if(!pass){
+        failed_variables_missing <- c(failed_variables_missing, variable)
+    }
+    pass <- all(dat_clean[[variable]] %in% c(0,1))
+    if(!pass){
+        failed_variables_01 <- c(failed_variables_01, variable)
+    }
 }
 
 if(length(failed_variables_missing) > 1){
-	stop(paste0("Unexpected missingness in: ", paste(failed_variables_missing, collapse = ", ")))	
+    stop(paste0("Unexpected missingness in: ", paste(failed_variables_missing, collapse = ", ")))   
 }
 
 if(length(failed_variables_missing) > 1){
-	stop(paste0("Unexpected values in: ", paste(failed_variables_01, collapse = ", ")))	
+    stop(paste0("Unexpected values in: ", paste(failed_variables_01, collapse = ", "))) 
 }
 
 ## at least some cases included in two phase sample
@@ -72,8 +73,8 @@ if(length(failed_variables_missing) > 1){
 pass57 <- sum(dat_clean$TwophasesampInd) > sum(dat_clean$SubcohortInd)
 pass29 <- sum(dat_clean$TwophasesampInd.2) > sum(dat_clean$SubcohortInd)
 if(!pass57){
-	stop("More people in subcohort than in final two-phase sample for Day 57 analysis.")
+    stop("More people in subcohort than in final two-phase sample for Day 57 analysis.")
 }
 if(!pass29){
-	stop("More people in subcohort than in final two-phase sample for Day 29 analysis.")
+    stop("More people in subcohort than in final two-phase sample for Day 29 analysis.")
 }

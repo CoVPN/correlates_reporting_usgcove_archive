bed8460b3b945e73715364ecacd0df8c1ac8c3be
Benkeser <benkeser@emory.edu>
2021-05-25 17:11:50 -0400

    update file path in make_raw_dat_check

--- data_clean/make_raw_dat_check.R
+++ data_clean/make_raw_dat_check.R
@@ -11,7 +11,9 @@ library(here)
 
 # load data and rename first column (ID)
 dat_proc <- read.csv(here(
-  "data_raw", data_in_file
+  "data_raw", 
+  ifelse(study_name == "ENSEMBLE", "janssen", "moderna"), # moderna placeholder for all other trials
+  data_in_file
 ))
 colnames(dat_proc)[1] <- "Ptid"
 

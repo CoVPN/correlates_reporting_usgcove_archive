3abc0abada553bdf91c3e52a5158a02ba30be631
Youyi Fong <youyifong@gmail.com>
2021-06-23 15:32:58 -0700

    misc updates for moderna correlates manus

--- cor_coxph/code/cor_coxph_marginalized_risk_bootstrap.R
+++ cor_coxph/code/cor_coxph_marginalized_risk_bootstrap.R
@@ -5,7 +5,7 @@
 # data: ph1 data
 # t: a time point near to the time of the last observed outcome will be defined
 marginalized.risk.svycoxph.boot=function(formula, marker.name, type, data, t, B, ci.type="quantile", numCores=1) {  
-# formula=form.0; marker.name="Day"%.%pop%.%a%.%"cat"; data=dat.vac.seroneg; t=t0; B=2; ci.type="quantile"; numCores=1; type=3
+# formula=form.0; marker.name="Day"%.%pop%.%a; data=dat.vac.seroneg; t=t0; B=2; ci.type="quantile"; numCores=1; type=3
     
     # store the current rng state 
     save.seed <- try(get(".Random.seed", .GlobalEnv), silent=TRUE) 
@@ -16,7 +16,7 @@ marginalized.risk.svycoxph.boot=function(formula, marker.name, type, data, t, B,
     if (type==1) {
     # conditional on s
         #ss contains 1) every 5% to include s1 and s2 for sensitivity analyses, 2) lars quantiles so that to be consistent with his analyses, and 3_ equally spaced values so that the curves look good  
-        ss=sort(c(quantile(data[[marker.name]], c(lars.quantiles,seq(.05,.95,by=.05)), na.rm=TRUE), seq(min(data[[marker.name]], na.rm=TRUE), max(data[[marker.name]], na.rm=TRUE), length=50)[-c(1,50)]))
+        ss=sort(c(report.assay.values(data[[marker.name]], marker.name.to.assay(marker.name)), seq(min(data[[marker.name]], na.rm=TRUE), max(data[[marker.name]], na.rm=TRUE), length=50)[-c(1,50)]))
         f1=update(formula, as.formula(paste0("~.+",marker.name)))        
         tmp.design=twophase(id=list(~1,~1), strata=list(NULL,~Wstratum), subset=~TwophasesampInd.0, data=data)
         fit.risk=try(svycoxph(f1, design=tmp.design)) # since we don't need se, we could use coxph, but the weights computed by svycoxph are a little different from the coxph due to fpc

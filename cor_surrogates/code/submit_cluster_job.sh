afaf61a6dd598e09027f9778c0c65a3e26bbcf6d
Bhavesh Borate <bborate@rhino02.fhcrc.org>
2021-06-27 14:04:53 -0700

    slight updates to documentation
    Former-commit-id: 2b79ab4ecb8d9745015864ae92df95cd7eeac00d

--- cor_surrogates/code/submit_cluster_job.sh
+++ cor_surrogates/code/submit_cluster_job.sh
@@ -2,6 +2,4 @@
 ml fhR/4.0.4-foss-2020b
 ml PyTorch/1.7.1-fosscuda-2020b
 ml jbigkit
-#sbatch -c10 --mem 11G --time=7-0 run_cvsl_with_riskscore_on_vaccinees.sh
-#sbatch -c10 --mem 11G --time=7-0  --array=1-13 --constraint=gizmok code/run_cvsl_varsets.sh
 sbatch -c10 --mem 11G --time=7-0 --constraint=gizmok code/run_cvsl_varsets.sh

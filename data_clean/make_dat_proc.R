30365567395e15dddd0a761e4db6f70e60941a47
Youyi Fong <youyifong@gmail.com>
2021-07-20 09:45:52 -0700

    comments only
    Former-commit-id: 8e811444d02e7e744930972b7373d0e7a37a5ee1

--- data_clean/make_dat_proc.R
+++ data_clean/make_dat_proc.R
@@ -23,6 +23,8 @@ dat_proc <- read.csv(here("data_raw", data_raw_dir, data_in_file))
 #hist(dat_proc$Day29bindN)
 #10**min(dat_proc$Day29bindN,na.rm=T)*0.0024*2
 
+
+
 if(study_name=="MockENSEMBLE") dat_proc=dat_proc[, !contain(names(dat_proc), "pseudoneutid")]
 
 colnames(dat_proc)[1] <- "Ptid" 

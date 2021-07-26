7a156519f6d98326f21cf5dbeb45f8e5979f8848
Youyi Fong <youyifong@gmail.com>
2021-07-26 05:45:31 -0700

    minor changes
    Former-commit-id: ec3c37ba7763fb33271e4b4ddaf6344439ce3b50

--- cor_graphical/code/cor_data_preprocess.R
+++ cor_graphical/code/cor_data_preprocess.R
@@ -1,4 +1,4 @@
-#Sys.setenv(TRIAL = "moderna_mock")
+#Sys.setenv(TRIAL = "janssen_pooled_real")
 #-----------------------------------------------
 # obligatory to append to the top of each script
 renv::activate(project = here::here(".."))

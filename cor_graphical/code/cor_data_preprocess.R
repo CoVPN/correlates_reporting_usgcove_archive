1461236e00a67eb09472fb42d1fba315d183d4b0
Lu <ylu2@fredhutch.org>
2021-07-26 18:05:01 -0700

    save process function in a new R file
    Former-commit-id: 4928bb9d4cfd1acdd33b67c55f552818cb9958c2

--- cor_graphical/code/cor_data_preprocess.R
+++ cor_graphical/code/cor_data_preprocess.R
@@ -5,6 +5,7 @@ renv::activate(project = here::here(".."))
 source(here::here("..", "_common.R"))
 #-----------------------------------------------
 
+source(here::here("code", "cor_process_function.R"))
 library(here)
 library(dplyr)
 library(tidyverse)
@@ -352,41 +353,6 @@ dat.longer.cor.subset$response = with(dat.longer.cor.subset, ifelse(assay %in% c
 #### figure specific data prep
 # 1. define response rate:
 # 2. make subsample datasets such that the violin plot only shows <= 100 non-case data points
-
-# a function to define response rate by group
-get_resp_by_group <- function(dat=dat, group=group){
-  
-  if(has57) {wt="wt.D57"} else {wt="wt.D29"}
-  
-  complete <- complete.cases(dat[, group])
-  
-  dat_resp_by_group <-
-    dat %>% filter(complete==1) %>%
-    group_by_at(group) %>%
-    mutate(counts = n(),
-           num = round(sum(response * ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)), na.rm=T), 1), # for intercurrent cases, we don't need to adjust for the weight because all of them are from the same stratum
-           denom = round(sum(ifelse(!cohort_event %in% c("Post-Peak Cases", "Non-Cases"), 1, !!as.name(wt)), na.rm=T), 1),
-           N_RespRate = paste0(counts, "\n",round(num/denom*100, 1),"%"),
-           min = min(value),
-           q1 = quantile(value, 0.25, na.rm=T),
-           median = median(value, na.rm=T),
-           q3 = quantile(value, 0.75, na.rm=T),
-           max= max(value))
-    
-  return(dat_resp_by_group)
-}
-
-# a function to get 1000 non-case sample by group for points & lines in line plot 
-get_sample_by_group <- function(dat.plot, group){
-
-  dat.sample <- dat.plot %>%
-    group_by_at(group) %>%
-    sample_n((ifelse(n()>=100 & cohort_event=="Non-Cases", 100, n())), replace=F) %>% filter(time=="Day 29") %>%
-    ungroup() %>%
-    select(c("Ptid", group[!group %in% "time"])) %>%
-    inner_join(dat.plot, by=c("Ptid", group[!group %in% "time"]))
-  return(dat.sample)
-} 
 
 #### for Figure 1. intercurrent vs pp, case vs non-case, (Day 1), Day 29 Day 57
 groupby_vars1=c("Trt", "Bserostatus", "cohort_event", "time", "assay")

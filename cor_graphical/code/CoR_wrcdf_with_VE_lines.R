ab70b38945231fc7b65dc23c4a4fcbcd18b5413a
Youyi Fong <youyifong@gmail.com>
2021-09-13 20:34:02 -0700

    Update CoR_wrcdf_with_VE_lines.R
    Former-commit-id: 97039b60e36b431030edda9acab450dcedd452c2

--- cor_graphical/code/CoR_wrcdf_with_VE_lines.R
+++ cor_graphical/code/CoR_wrcdf_with_VE_lines.R
@@ -51,7 +51,7 @@ for (bstatus in 0:1) {
       # use ve computed as part of cor_coxph, which is defined based on marginalized risks
       load(paste0(here::here("..", "cor_coxph", "output"), "/",
                   attr(config,"config"),
-                  "/PrimaryD57/marginalized.risk.no.marker."%.%study_name%.%".Rdata"))
+                  "/D57/marginalized.risk.no.marker."%.%study_name%.%".Rdata"))
       VE <- overall.ve[1]
       VE_lb <- overall.ve[2]
       VE_ub <- overall.ve[3]
@@ -86,7 +86,7 @@ for (bstatus in 0:1) {
       # use ve computed as part of cor_coxph, which is defined based on marginalized risks
       load(paste0(here::here("..", "cor_coxph", "output"), "/",
                   attr(config,"config"),
-                  "/PrimaryD29/marginalized.risk.no.marker."%.%study_name%.%".Rdata"))
+                  "/D29/marginalized.risk.no.marker."%.%study_name%.%".Rdata"))
       VE <- overall.ve[1]
       VE_lb <- overall.ve[2]
       VE_ub <- overall.ve[3]
@@ -136,7 +136,7 @@ for (bstatus in 0:1) {
       # use ve computed as part of cor_coxph, which is defined based on marginalized risks
       load(paste0(here::here("..", "cor_coxph", "output"), "/",
                   attr(config,"config"),
-                  "/PrimaryD29/marginalized.risk.no.marker."%.%study_name%.%".Rdata"))
+                  "/D29/marginalized.risk.no.marker."%.%study_name%.%".Rdata"))
       VE <- overall.ve[1]
       VE_lb <- overall.ve[2]
       VE_ub <- overall.ve[3]

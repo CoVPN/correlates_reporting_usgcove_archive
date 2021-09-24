ef309351f6e54b06ad1878cbd948dbc87ddb9be9
Youyi Fong <youyifong@gmail.com>
2021-09-24 16:42:54 -0700

    use international units for assay in the mock input, skip conversion in data processing; generalize table notes and captions
    Former-commit-id: 29dd4f053befdda1bd3e6df132385ff175c727f7

--- data_clean/make_dat_proc.R
+++ data_clean/make_dat_proc.R
@@ -519,15 +519,16 @@ if(has29) {
 assays.includeN=c(assays, "bindN")
 
 
-###############################################################################
-# converting binding variables from AU to IU for binding assays
-###############################################################################
-
-for (a in assays.includeN) {
-  for (t in c("B", if(has29) "Day29", if(has57) "Day57") ) {
-      dat_proc[[t %.% a]] <- dat_proc[[t %.% a]] + log10(convf[a])
-  }
-}
+################################################################################
+## converting binding variables from AU to IU for binding assays
+## THIS IS NO LONGER NEEDED AS WE EXPECT THE INPUT DATA TO BE ON INTERNATIONAL UNIT SCALES
+################################################################################
+#
+#for (a in assays.includeN) {
+#  for (t in c("B", if(has29) "Day29", if(has57) "Day57") ) {
+#      dat_proc[[t %.% a]] <- dat_proc[[t %.% a]] + log10(convf[a])
+#  }
+#}
 
 
 ###############################################################################

2e7979040b43d73ab940cb6426cbd07a540812af
Youyi Fong <youyifong@gmail.com>
2021-09-27 11:14:41 -0700

    Revert "update marker data to international units in mock data, remove conversion from data processing; generalize captions and notes using config"

--- cor_coxph/code/params.R
+++ cor_coxph/code/params.R
@@ -2,16 +2,15 @@ numCores <- unname(ifelse(Sys.info()["sysname"] == "Windows",
                           1, future::availableCores()))
 
 
-#if (length(assays) %in% c(3,4)) {
-#  .mfrow <- c(2, 2)
-#} else if (length(assays) == 5) {
-#  .mfrow <- c(3, 2)
-#} else if (length(assays) == 2) {
-#  .mfrow <- c(1, 2)
-#} else {
-#  stop("Please re-define variable .mfrows")
-#}
-.mfrow <- c(1, 1)
+if (length(assays) %in% c(3,4)) {
+  .mfrow <- c(2, 2)
+} else if (length(assays) == 5) {
+  .mfrow <- c(3, 2)
+} else if (length(assays) == 2) {
+  .mfrow <- c(1, 2)
+} else {
+  stop("Please re-define variable .mfrows")
+}
 
 
 # need this function b/c svycoxh may error due to singularity if, e.g. all cases have the same marker value
--- data_clean/make_dat_proc.R
+++ data_clean/make_dat_proc.R
@@ -521,15 +521,12 @@ assays.includeN=c(assays, "bindN")
 
 ###############################################################################
 # converting binding variables from AU to IU for binding assays
-# THIS IS NO LONGER NEEDED AS WE EXPECT THE INPUT DATA TO BE ON INTERNATIONAL UNIT SCALES
 ###############################################################################
 
-if(study_name=="MockCOVE" | study_name=="MockENSEMBLE"){
-    for (a in assays.includeN) {
-      for (t in c("B", if(has29) "Day29", if(has57) "Day57") ) {
-          dat_proc[[t %.% a]] <- dat_proc[[t %.% a]] + log10(convf[a])
-      }
-    }
+for (a in assays.includeN) {
+  for (t in c("B", if(has29) "Day29", if(has57) "Day57") ) {
+      dat_proc[[t %.% a]] <- dat_proc[[t %.% a]] + log10(convf[a])
+  }
 }
 
 
